05f4d2378e07f38071f2a5568bece9aa02049211
timber <624322575@qq.com>
2021-08-01 23:11:35 +0800

    Update algorithms.cpp

--- leetcode/algorithms.cpp
+++ leetcode/algorithms.cpp
@@ -26,6 +26,7 @@
 题目24：前K个高频单词（哈希表+优先队列）
 题目25：奇怪的打印机（动态规划）
 题目26：数组中最大数对和的最小值（排序+贪心）
+题目27：避免洪水泛滥（贪心+二分）
 =========================================*/
 
 
@@ -1524,5 +1525,46 @@ public:
             res = max(res, nums[i] + nums[n - 1 - i]);
         }
         return res;
+    }
+};
+
+/*-------------------------------
+| 题目27：避免洪水泛滥（贪心+二分）
+| 你的国家有无数个湖泊，所有湖泊一开始都是空的。当第 n 个湖泊下雨的时候，如果第 n 个湖泊是空的，那么它就会装满水，否则这个湖泊会发生洪水。你的目标是避免任意一个湖泊发生洪水。
+| 给你一个整数数组 rains ，其中：
+| rains[i] > 0 表示第 i 天时，第 rains[i] 个湖泊会下雨。
+| rains[i] == 0 表示第 i 天没有湖泊会下雨，你可以选择 一个 湖泊并 抽干 这个湖泊的水。
+| 请返回一个数组 ans ，满足：
+| ans.length == rains.length
+| 如果 rains[i] > 0 ，那么ans[i] == -1 。
+| 如果 rains[i] == 0 ，ans[i] 是你第 i 天选择抽干的湖泊。
+| 如果有多种可行解，请返回它们中的 任意一个 。如果没办法阻止洪水，请返回一个 空的数组 。
+| 请注意，如果你选择抽干一个装满水的湖泊，它会变成一个空的湖泊。但如果你选择抽干一个空的湖泊，那么将无事发生（详情请看示例 4）。
+-------------------------------*/
+/* 贪心+二分查找 */
+class Solution {
+public:
+    vector<int> avoidFlood(vector<int>& rains) {
+        vector<int> ans(rains.size(), 1); //结果数组
+        unordered_map<int, int> water; //记录每个湖泊上一次下雨的日期
+        set<int> zero; //记录晴天
+        for (int i = 0; i < rains.size(); i++) {
+            int rain = rains[i];
+            if (rain == 0) { //处理晴天
+                zero.insert(i); //把晴天插入到晴天集合
+                continue;
+            }
+            if (water.count(rain) > 0) {
+                //二分查找（>=），在上一次下雨之后的晴天
+                auto it = zero.lower_bound(water[rain]);
+                //找不到，就会引发洪水
+                if (it == zero.end()) return {};
+                ans[*it] = rain; //需要用指针，因为it是地址
+                zero.erase(it); //该晴天已用，从晴天集合中擦除
+            }
+            water[rain] = i; //更新上一次下雨的日期
+            ans[i] = -1; //当天记录为雨天
+        }
+        return ans;
     }
 };
