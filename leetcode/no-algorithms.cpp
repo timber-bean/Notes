3a3e446e677d0652ae9038b12bc4b37def0fa5cb
timber <624322575@qq.com>
2021-08-15 19:44:16 +0800

    Update no-algorithms.cpp

--- leetcode/no-algorithms.cpp
+++ leetcode/no-algorithms.cpp
@@ -23,6 +23,7 @@
 题目21：剪绳子（数学推理）
 题目22：等差数列划分
 题目23：替换空格（原地修改）
+题目24：用两个栈实现队列
 =========================================*/
 
 
@@ -875,3 +876,43 @@ public:
         return s;
     }
 };
+
+/*-------------------------------
+| 题目24：用两个栈实现队列
+| 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+| 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+-------------------------------*/
+/* O(N) O(N) */
+class CQueue {
+private:
+    stack<int> s1; //用于在尾部添加元素
+    stack<int> s2; //用于删除头部元素
+public:
+    CQueue() {}
+    
+    void appendTail(int value) {
+        s1.push(value);
+    }
+    
+    int deleteHead() {
+        //s2为空的时候才从s1获取元素压入s2
+        //否则，s2中的元素就是按顺序待删除的头部元素
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        if(s2.empty()) return -1;
+        int ans = s2.top();
+        s2.pop();
+        return ans;
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
