be8938ab0874a8617aa3192ac188944c7b662a8d
timber <624322575@qq.com>
2021-08-23 22:43:03 +0800

    0823

--- leetcode/algorithms.cpp
+++ leetcode/algorithms.cpp
@@ -47,6 +47,7 @@
 题目45：无重复字符的最长子串（双指针）
 题目46：复杂链表的复制（拼接+拆分）
 题目47：二叉搜索树与双向链表（DFS中序遍历）
+题目48：旋转数组的最小数字（二分查找）
 =========================================*/
 
 
@@ -2321,5 +2322,29 @@ public:
         head->left = pre;
         pre->right = head;
         return head;
+    }
+};
+
+/*-------------------------------
+| 题目48：旋转数组的最小数字（二分查找）
+| 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+| 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+| 例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+-------------------------------*/
+/* 二分查找 O(logN) O(1) */
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int low = 0, high = numbers.size()-1;
+        while(low < high){
+            int middle = low + (high - low) / 2; //取中间元素
+            //高置位大于中间元素，则高置位降至中间
+            if(numbers[high] > numbers[middle]) high = middle;
+            //高置位小于中间元素，则低置位升至中间+1的位置
+            else if(numbers[high] < numbers[middle]) low = middle + 1;
+            //若相等，则高置位-1
+            else --high;
+        }
+        return numbers[low]; //低置位元素即是最小
     }
 };
